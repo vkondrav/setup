1f113fa6863fad5f32b430f25219014fc70b087b
Vitaliy Kondratiev <kondrav@live.ca>
2013-07-16 22:31:46 -0400

    added "nodejs"

--- setup.sh
+++ setup.sh
@@ -16,6 +16,8 @@ nvm use v0.10.12
 # http://jshint.com/
 npm install -g jshint
 
+sudo apt-get install nodejs
+
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
